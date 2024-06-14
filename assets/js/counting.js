98dbb8def65d50a6ac2a853b86b3176dca3e68d3
yym0807 <75062543+yym0807@users.noreply.github.com>
2024-06-14 21:18:39 +0800

    Update counting.js

--- assets/js/counting.js
+++ assets/js/counting.js
@@ -5,8 +5,16 @@ $(() => {
 
     $('#add').on('click', () => {
         var c = $('#num').val()
-        console.log(c)
-
-        $('#ress').val(c)
+        if(!isNaN(c) && c < 100){
+            Array[c]++
+            $('#ress').val("數字列表")
+            for(var i = 0; i < 100; i++){
+                if(Array[i] > 0){
+                    var D = document.createElement('div')
+                    D.innerHTML = i + ": " + Array[i]
+                    $('#ress').appendChild(D)
+                }
+            }
+        }
     })
 })

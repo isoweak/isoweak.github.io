d3c078f647baf13271c27a8aa97005b3497e6ab9
yym0807 <75062543+yym0807@users.noreply.github.com>
2024-06-15 15:18:28 +0800

    Update counting.js

--- assets/js/counting.js
+++ assets/js/counting.js
@@ -27,11 +27,8 @@ $(() => {
         var r = $('#num').val()
         if(r != ""){
             c = parseInt(r)
-            if(c < 100 && c >= 0){
-                console.log(c===1)
-                console.log(c==='1')
-                console.log(Array[c])
-                if(Array[c] > 0) Array[c]--
+            if(c < 100 && c >= 0 && Array[c] > 0){
+                Array[c]--
                 $('#ress').empty()
                 for(var i = 0; i < 100; i++){
                     if(Array[i] > 0){
